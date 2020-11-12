c891250f4e37745599658e9301bc1d5d2b356ae3
Jacob Hensley <jacobrhensley3@gmail.com>
2020-11-12 10:34:10 -0600

    created a for loop practice class in java

---
+++ src/loopsPackage/forLoops.java
@@ -0,0 +1,16 @@
+public class forLoops {
+
+    public static void main(String[] args) {
+        
+        // String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        // for (int i = alphabet.length() -1; i >= 0; i--){
+        //     System.out.println("Char: " + alphabet.charAt(i));
+        // }
+
+        for (int i = 0; i <= 100; i+=2){
+            System.out.println("Even Number: " + i);
+        }
+    }
+    
+}
